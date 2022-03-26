42a7649897b8951c89e98cc465ba7f88e7185866
Thiago <Thiagolopespereiralp1@gmail.com>
2022-03-25 21:50:03 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double peso, altura, imc;
+
+Console.WriteLine("Altura (m)..: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Peso (kg)...: ");
+peso = Convert.ToDouble(Console.ReadLine());
+
+imc = peso / Math.Pow(altura, 2);
+
+Console.WriteLine($"\nIMC: {imc:N1} kg/m²");
